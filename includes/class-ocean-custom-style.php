946c7224d95842c7135d3d215d5b8dd6e80c564a
younes-dro <dro66_8@hotmail.fr>
2020-09-27 15:33:29 +0100

    Hide the page title and keep the breadcrums

--- includes/class-ocean-custom-style.php
+++ includes/class-ocean-custom-style.php
@@ -9,36 +9,45 @@
  */
 class Ocean_Custom_Style {
 
-    public function __construct(){
-        add_filter( 'ocean_display_page_header', array ( $this , 'disable_title' ) );
-        add_filter( 'ocean_post_layout_class', array ( $this , 'product_layout_class' ), 20 );
+    public function __construct() {
+//        add_filter( 'ocean_display_page_header', array ( $this , 'disable_title' ) );
+        add_filter('ocean_post_layout_class', array($this, 'product_layout_class'), 20);
+        add_filter('ocean_display_page_header_heading', array($this, 'prefix_page_header_heading'));
 //        add_filter( 'ocean_main_metaboxes_post_types', array ( $this , 'oceanwp_metabox' ), 20 );       
     }
-    public function disable_title( $return ){
+
+    public function disable_title($return) {
         global $post;
-        if ( $post->post_type == 'tecnibo_project' || $post->post_type == 'tecnibo_product' || is_page_template( 'projects-page.php' ) ) {
+        if ($post->post_type == 'tecnibo_project' || $post->post_type == 'tecnibo_product' || is_page_template('projects-page.php')) {
             $return = false;
         }
-        
-        return $return;        
-    }
-    public function product_layout_class (){
-        global $post;
-        if ( $post->post_type == 'tecnibo_project' || $post->post_type == 'tecnibo_product' || is_page_template( 'projects-page.php' ) ) {	
-		$class = 'full-width';
-	}
 
-	// Return correct class
-	return $class;        
+        return $return;
     }
+
+    public function product_layout_class() {
+        global $post;
+        if ($post->post_type == 'tecnibo_project' || $post->post_type == 'tecnibo_product' || is_page_template('projects-page.php')) {
+            $class = 'full-width';
+        }
+
+        // Return correct class
+        return $class;
+    }
+
+    public function prefix_page_header_heading($return) {
+        $return = false;
+    }
+
     /**
      * Add the OceanWP Settings metabox in your CPT
      */
-    public function oceanwp_metabox( $types ){
-	// Your custom post type
-	$types[] = 'tecnibo_product';
+    public function oceanwp_metabox($types) {
+        // Your custom post type
+        $types[] = 'tecnibo_product';
 
-	// Return
-	return $types;   
+        // Return
+        return $types;
     }
+
 }
