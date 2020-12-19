715d52ac353c248221de0eddbf953e2e06247d89
younes-dro <dro66_8@hotmail.fr>
2020-12-19 20:20:35 +0100

    Add custom slick arrow

--- assets/tecnibo-front.js
+++ assets/tecnibo-front.js
@@ -3,7 +3,10 @@
  */
 ;
 (function ($) {
-
+        $('.project-photos, .product-photos, .tecnibo-customsers ').on('init', function(event, slick){
+//            $(this).find('button.slick-next').addClass('fa fa-chevron-right');
+//            $(this).find('button.slick-prev').addClass('fa fa-chevron-left');
+        });
     function getSliderSettings() {
         return {
             dots: false,
@@ -11,6 +14,8 @@
             speed: 300,
             slidesToShow: 4,
             slidesToScroll: 4,
+            prevArrow: '<button type="button" class="slick-prev fa fa-chevron-left">Previous</button>',
+            nextArrow: '<button type="button" class="slick-next fa fa-chevron-right">Next</button>',
             responsive: [
                 {
                     breakpoint: 1024,
@@ -48,6 +53,8 @@
             arrows: true,
             speed: 300,
             slidesPerRow: 4,
+            prevArrow: '<button type="button" class="slick-prev fa fa-chevron-left">Previous</button>',
+            nextArrow: '<button type="button" class="slick-next fa fa-chevron-right">Next</button>',            
             responsive: [
                 {
                     breakpoint: 1024,
@@ -74,7 +81,11 @@
         $('.project-photos').slickLightbox({
             background: 'rgb(255 255 255)',
             src: 'src',
-            itemSelector: '.projetc-item-image  img'
+            itemSelector: '.projetc-item-image  img',
+            slick:{
+                prevArrow: '<button type="button" class="slick-prev fa fa-chevron-left">Previous</button>',
+                nextArrow: '<button type="button" class="slick-next fa fa-chevron-right">Next</button>',
+            }
         });
 
         $('#tecnibo-canditate').click(function () {
